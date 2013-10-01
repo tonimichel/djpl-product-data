3ea0fa045eb419a3d1e255a196545020f0c4e8ed
henzk <speidel@schnapptack.de>
2013-10-01 20:05:05 +0200

    introduced DATA_DIR

--- product_data/settings.py
+++ product_data/settings.py
@@ -1,7 +1,9 @@
 import os
 from django_productline.context import PRODUCT_CONTEXT
 
+
 DATA_DIR = os.path.join(PRODUCT_CONTEXT.PRODUCT_DIR, '__data__/')
+introduce_DATA_DIR = DATA_DIR
 
 if not os.path.exists(DATA_DIR):
     os.mkdir(DATA_DIR)
